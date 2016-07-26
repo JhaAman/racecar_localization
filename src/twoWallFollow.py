7b16cc13738f28dcdbe404392a34ab04d4d22c3c
Clara <clarali2000@126.com>
2016-07-26 20:51:46 +0000

    did more driving stuff

--- src/twoWallFollow.py
+++ src/twoWallFollow.py
@@ -6,26 +6,55 @@ from sensor_msgs.msg import LaserScan
 import std_msgs
 import math
 from PID import PIDController
-
-PID_KP = 0.01
-SLICE_LEN = 40
+#MEMES BRO
+PID_KP = 0.0045
+PID_KP_countclock = 0.0035
+PID_KI = 0.0
+PID_KD = 0.0
+SLICE_LEN = 50
 class twoWallFollow:
     def __init__(self):
-        rospy.Subscriber('racecar/laser/scan', LaserScan, self.laser_callback, queue_size=10)
-        self.publisher = rospy.Publisher('/racecar/ackermann_cmd_mux/input/navigation', AckermannDriveStamped, queue_size=10)
+        rospy.Subscriber('/scan', LaserScan, self.laser_callback, queue_size=10)
+        self.publisher = rospy.Publisher('/vesc/ackermann_cmd_mux/input/navigation', AckermannDriveStamped, queue_size=10)
         self.drive_msg = AckermannDriveStamped()
-        self.pid = PIDController(rospy.Time.now(),  PID_KP)
+        self.pid = PIDController(rospy.Time.now(),  PID_KP,PID_KI,PID_KD)
         self.publish()
 
     def laser_callback(self, msg):
-        averaged = [sum(msg.ranges[i:i+40])/40 for i in range(0, 1080, 40)]
-        furthest = max(enumerate(averaged),  key=lambda x: x[1])[0]
-        targetPoint = furthest * 40 + 20
-        
+	ranges = [1 if msg.ranges[i] > 1 else 0 for i in range(1081)]
+	rangeStarted = False
+	currStart = None
+	currEnd = None
+	currLen = None
+	maxLen = None
+	maxEnd = None
+	maxStart = None
+	for i in range(len(ranges)):
+	    if ranges[i]:
+		if rangeStarted:
+			pass
+		else:
+			currStart = i
+			rangeStarted = True
+	    else:
+		if rangeStarted:
+			currEnd = i
+			currLen = currEnd - currStart
+			if currLen > maxLen:
+				maxLen = currLen
+				maxStart = currStart
+				maxEnd = currEnd
+			rangeStarted = False
+			
+		 	
+	
+
+        targetPoint = (maxEnd - maxStart)/2
+        print targetPoint
         error = (targetPoint - 540)/4
         steer_angle = self.pid.update(error,  rospy.Time.now())
         self.drive_msg = AckermannDriveStamped()
-        self.drive_msg.drive.speed = 1.0
+        self.drive_msg.drive.speed = 2.0
         self.drive_msg.drive.steering_angle = steer_angle
 
     def publish(self):
