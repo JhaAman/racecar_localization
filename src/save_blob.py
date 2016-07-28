436d94170e4cd2374e21af83b1d2e71935925154
clareesi <clarali2000@126.com>
2016-07-28 02:08:41 -0400

    debugged blob saving, need to fix pathing and adjust color values

--- src/save_blob.py
+++ src/save_blob.py
@@ -31,6 +31,7 @@ class ColorTracker:
         self.bridge = CvBridge()
 
         rospy.loginfo("[%s] Initialized." %(self.node_name))
+        #mkdir("~/challenge_photos")
 
     def cbImage(self,image_msg):
         thread = threading.Thread(target=self.processImage,args=(image_msg,))
@@ -39,15 +40,15 @@ class ColorTracker:
 
     def detection(self, img):
 
-        bounds = [["green", [54, 30, 60], [72, 255, 255]], ["red", [0, 160, 130], [15, 255, 255]]]
+        bounds = [["green", [54, 30, 60], [72, 255, 255]], ["red", [0, 180, 150], [15, 255, 255]], ["blue", [200, 50, 45], [210, 90, 90]], ["yellow", [25, 35, 140], [40, 255, 255]], ["racecar", [0, 0, 0], [0, 0, 0]], ["ari", [0, 0, 0], [0, 0, 0]], ["professor karaman", [0, 0, 0], [0, 0, 0]], ["cat", [0, 0, 0], [0, 0, 0]]]
         
         for i in bounds:
-            lower = bounds[i][1]
-            upper = bounds[i][2]
-            color = bounds[i][0]
+            lower = np.array(i[1])
+            upper = np.array(i[2])
+            color = i[0]
 
             ret = self.detect_color_blob(img, lower, upper, color)
-            color_code = i+1
+            color_code = bounds.index(i)+1
 
         if ret == None:
             cx = 0
@@ -76,11 +77,9 @@ class ColorTracker:
 
         contours, h = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        if self.debugging:
-            cv2.drawContours(img, contours, -1, (0, 0, 255), 2)
-            self.saveImage(img)
-            self.notification = "I see"+color
-
+        #if self.debugging:
+            #cv2.drawContours(img, contours, -1, (0, 0, 255), 2)
+                                  
         sorted_contours = sorted(contours, key = lambda c: cv2.contourArea(c), reverse=True)
 
         if len(sorted_contours) < 1:
@@ -100,7 +99,7 @@ class ColorTracker:
 
 
         if self.debugging:
-            cv2.drawContours(img, [c], -1, (255, 0, 0), 3) 
+            #cv2.drawContours(img, [c], -1, (255, 0, 0), 3) 
             cv2.drawContours(img, [approx], -1, (0, 255, 0), 5) 
 
             coord = (approx[0][0][0], approx[0][0][1])
@@ -108,6 +107,11 @@ class ColorTracker:
 
         M = cv2.moments(approx)
         cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+
+        self.notification = "I see "+color
+        #self.photo_iter = 0
+        #self.photo_timer = rospy.Timer(rospy.Duration(0.5), self.saveImage(img))
+        self.saveImage(img)
 
         if self.debugging:
             cv2.circle(img, (cx, cy), 10, (255, 255, 255), -1)
@@ -117,15 +121,23 @@ class ColorTracker:
         return (cx, cy, approx_area)
 
     def saveImage(self, img):
-        path = "~/home/racecar/challenge_photos/"+self.image_count+".png"
+        #self.photo_iter += 1
+        #if self.photo_iter > 5:
+
+        #NEED TO FIX PATHING
+        path = str(self.image_count)+".png"
+        print path
         cv2.imwrite(path, img)
+        self.image_count += 1
+        #self.photo_timer.shutdown()
+        
 
     def processImage(self, image_msg):
         if not self.thread_lock.acquire(False):
             return
         image_cv = self.bridge.imgmsg_to_cv2(image_msg)
 
-        self.detection(image_cv)
+        self.detection(image_cv )
         
         if self.debugging:
             try:
