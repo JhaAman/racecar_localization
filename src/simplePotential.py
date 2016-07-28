b361cf0786ba45aa9ffa7935a8e78d448f97a305
Caleb Trotz <caleb.trotz@gmail.com>
2016-07-27 22:53:23 -0400

    potential field values still not great

--- src/simplePotential.py
+++ src/simplePotential.py
@@ -5,42 +5,47 @@ from ackermann_msgs.msg import AckermannDriveStamped
 from sensor_msgs.msg import LaserScan
 import std_msgs
 import math
-
-def __init__(self):
-    rospy.Subscriber('/scan', LaserScan, self.scan_callback, queue_size=10)
-    self.publisher = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=10)
-    self.drive_msg = AckermannDriveStamped()
-    self.publish()
-
-def scan_callback(self, msg):
-    scanRadAngles = ( (msg.angle_increment * np.arrange(1801,dtype = float) + msg.angle_min)
-    scanXUnitVectors = -np.cos(scanRadAngles)
-    scanYUnitVectors = -np.sin(scanRadAngles)
-
-    scanXComponent = (self.charge_laser_particle * scanXUnitVectors) / np.square(msg.ranges)
-    scanYComponent = (self.charge_laser_particle * scanYUnitVectors) / np.square(msg.ranges)
-
-    kickXComponent = np.ones(1) * self.charge_forward_boost/self.boost_distance**2.0
-    kickYComponent = np.zeros(1)
-
-    totalXComponent = np.sum(scanXComponent) * kickXComponent
-    totalYComponent = np.sum(scanYComponent) * kickYComponent
-
-    """visualizer_msg = PointStamped()
-    visualizer_msg.header.frame_id = 'base_link'
-    visualizer_msg.point.x = total_x_component
-    visualizer_msg.point.y = total_y_component
-
-    self.pub_goal.publish(visualizer_msg)"""
-    self.drive_msg = AckermannDriveStamped()
-    self.drive_msg.drive.speed = 0.1*np.sqrt((totalXComponent)*(totalXComponent)+(totalYComponent)*(totalYComponent)) * np.sign(totalXComponent)
-    self.drive_msg.drive.steering_angle = math.atan2(totalYComponent, totalXComponent) * np.sign(totalXComponent)
-
-def publish(self):
-    while not rospy.is_shutdown():
-        self.publisher.publish(self.drive_msg)
-        rospy.Rate(8).sleep()
+import numpy as np
+class simplePotential:
+    def __init__(self):
+        rospy.Subscriber('racecar/laser/scan', LaserScan, self.scan_callback, queue_size=10)
+        self.publisher = rospy.Publisher('/racecar/ackermann_cmd_mux/input/navigation', AckermannDriveStamped, queue_size=10)
+        self.drive_msg = AckermannDriveStamped()
+        self.charge_laser_particle = 0.00001
+        self.charge_forward_boost = 50
+        self.boost_distance = 1.0
+        self.publish()
+    
+    def scan_callback(self, msg):
+        scanRadAngles = (msg.angle_increment * np.arange(1080,dtype = float) + msg.angle_min)
+        scanXUnitVectors = -np.cos(scanRadAngles)
+        scanYUnitVectors = -np.sin(scanRadAngles)
+    
+        scanXComponent = (self.charge_laser_particle * scanXUnitVectors) / np.square(msg.ranges)
+        scanYComponent = (self.charge_laser_particle * scanYUnitVectors) / np.square(msg.ranges)
+    
+        kickXComponent = np.ones(1) * self.charge_forward_boost/self.boost_distance**2.0
+        kickYComponent = np.zeros(1)
+    
+        totalXComponent = np.sum(scanXComponent) * kickXComponent
+        totalYComponent = np.sum(scanYComponent) * kickYComponent
+    
+        """visualizer_msg = PointStamped()
+        visualizer_msg.header.frame_id = 'base_link'
+        visualizer_msg.point.x = total_x_component
+        visualizer_msg.point.y = total_y_component
+    
+        self.pub_goal.publish(visualizer_msg)"""
+        self.drive_msg = AckermannDriveStamped()
+        self.drive_msg.drive.speed = np.sqrt((totalXComponent)*(totalXComponent)+(totalYComponent)*(totalYComponent)) * np.sign(totalXComponent)
+        self.drive_msg.drive.steering_angle = 0.1*math.atan2(totalYComponent, totalXComponent) * np.sign(totalXComponent)
+        print self.drive_msg.drive.steering_angle
+    
+    def publish(self):
+        while not rospy.is_shutdown():
+            self.publisher.publish(self.drive_msg)
+            rospy.Rate(8).sleep()
 if __name__ == "__main__":
     rospy.init_node("TwoWallFollow")
-    e = twoWallFollow()
+    e = simplePotential()
     rospy.spin()
